689c2fc4146ce8e1bc7945ca001701234b2622c2
hahahanDW <gksehddn8@naver.com>
2023-05-10 20:39:21 +0900

    글쓰기 버튼 누르면 글쓰기 페이지 이동

--- app/src/main/java/com/example/gonggu/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gonggu/ui/home/HomeFragment.kt
@@ -2,9 +2,11 @@ package com.example.gonggu.ui.home
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,11 +14,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gonggu.Data.ChatDataList
 import com.example.gonggu.Data.HomeDataList
+import com.example.gonggu.MainActivity
 import com.example.gonggu.R
 import com.example.gonggu.databinding.FragmentHomeBinding
 import com.example.gonggu.ui.chat.ChatFragment
 import com.example.gonggu.ui.chat.ChatListAdapter
 import com.example.gonggu.ui.chat.RecyclerDecoration
+import com.example.gonggu.ui.post.PostFragment
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
@@ -42,6 +46,11 @@ class HomeFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_home,container,false)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView_realtimelist)
+        val wrBtn = view.findViewById<Button>(R.id.wrBtn) // 글쓰기 버튼
+        val mActivity = activity as MainActivity
+        wrBtn.setOnClickListener {
+            mActivity.replaceFragment(PostFragment())
+        }
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = ChatListAdapter(ChatDataList)
         val spaceDecoration = RecyclerDecoration(40)
