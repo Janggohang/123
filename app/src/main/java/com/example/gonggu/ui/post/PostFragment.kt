15b20bb1cb329dff28f66dd9618c1a31c781dbc8
hahahanDW <gksehddn8@naver.com>
2023-05-11 02:06:28 +0900

    게시글 파이어베이스 등록 확인
    임시 데이터 저장 완

--- app/src/main/java/com/example/gonggu/ui/post/PostFragment.kt
+++ app/src/main/java/com/example/gonggu/ui/post/PostFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.gonggu.R
+import com.example.gonggu.databinding.FragmentPostBinding
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class PostFragment : Fragment() {
+    lateinit var binding: FragmentPostBinding
+    val db = Firebase.database
+    val postsRef = db.getReference("post")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +23,26 @@ class PostFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false)
-    }
+        binding = FragmentPostBinding.inflate(inflater, container, false)
 
+        val summitBtn = binding!!.submitButton
+        summitBtn.setOnClickListener {
+            registerPost()
+        }
+        return binding!!.root
+    }
+    private fun registerPost() { // 게시물 등록
+        val title = binding!!.titleEdit.text.toString() // 제목
+        val price = binding!!.priceEdit.text.toString().toInt() // 가격
+        val numOfPeople = binding!!.countEdit.text.toString().toInt() // 인원 수
+        val content = binding!!.contentEdit.text.toString() // 내용
+        val itemMap = hashMapOf(
+            "content" to content,
+            "numOfPeople" to numOfPeople,
+            "price" to price,
+            "title" to title
+        )
+        val postRef = postsRef.push()
+        postRef.setValue(itemMap)
+    }
 }
