0fd7ad0e4ed0ad8bd4c998a31afd2a921a236131
hahahanDW <gksehddn8@naver.com>
2023-05-10 18:46:47 +0900

    manifest 파일 수정
    manifest 파일 수정

--- app/src/main/java/com/example/gonggu/ui/location/LocationFragment.kt
+++ app/src/main/java/com/example/gonggu/ui/location/LocationFragment.kt
@@ -1,6 +1,8 @@
 package com.example.gonggu.ui.location
 
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.location.LocationListener
 import android.location.LocationManager
 import android.util.Log
 import android.os.Bundle
@@ -8,6 +10,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.gonggu.R
 import com.example.gonggu.MainActivity
 import com.example.gonggu.databinding.FragmentLocationBinding
@@ -34,6 +37,7 @@ class LocationFragment : Fragment(), MapView.MapViewEventListener {
         binding = FragmentLocationBinding.inflate(inflater, container, false)
         val root : View = binding!!.root
         map = MapView(context)
+        //requestLocationUpdates()
         binding!!.mapView.addView(map)
         startTracking()
         map.setMapViewEventListener(this)
@@ -54,13 +58,13 @@ class LocationFragment : Fragment(), MapView.MapViewEventListener {
         val uLatitude = userNowLocation?.latitude
         val uLongitude = userNowLocation?.longitude
         //val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!)
+        Log.d("activity2", "latitude : $uLatitude, longitude : $uLongitude")
         // 중심점 변경
         //map.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!), true)
         val uNowPosition = MapPoint.mapPointWithGeoCoord(37.5828, 127.0106)
         map.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.5828, 127.0106), true)
         Log.d("location", "위도: $uLatitude 경도: $uLongitude")
         // 현위치에 마커 찍기
-
         marker.itemName = "현 위치"
         marker.tag = 0
         marker.mapPoint = uNowPosition
@@ -76,6 +80,23 @@ class LocationFragment : Fragment(), MapView.MapViewEventListener {
     override fun onDestroy() {
         super.onDestroy()
         stopTasking()
+    }
+
+    private fun requestLocationUpdates() {
+        val locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        val locationListener = LocationListener { location ->
+            val latitude = location.latitude
+            val longitude = location.longitude
+            Log.d("activity", "latitude : $latitude, longitude : $longitude")
+        }
+
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            1000L,
+            10.0F,
+            locationListener
+        )
     }
 
     override fun onMapViewInitialized(p0: MapView?) {
