3bd7567e74f96a179a2b56dd2a307b4476b6b907
hahahanDW <gksehddn8@naver.com>
2023-05-11 15:27:40 +0900

    내 위치 설정
    카카오맵에서 내 위치 설정

--- app/src/main/java/com/example/gonggu/ui/location/LocationFragment.kt
+++ app/src/main/java/com/example/gonggu/ui/location/LocationFragment.kt
@@ -1,8 +1,8 @@
 package com.example.gonggu.ui.location
 
-import android.content.Context
 import android.content.Context.LOCATION_SERVICE
-import android.location.LocationListener
+import android.location.Geocoder
+import android.location.Location
 import android.location.LocationManager
 import android.util.Log
 import android.os.Bundle
@@ -10,21 +10,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getSystemService
-import com.example.gonggu.R
-import com.example.gonggu.MainActivity
 import com.example.gonggu.databinding.FragmentLocationBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+import java.io.IOException
+import kotlin.properties.Delegates
 
-
-
-class LocationFragment : Fragment(), MapView.MapViewEventListener {
+class LocationFragment : Fragment() {
 
     private var binding: FragmentLocationBinding? = null
     private lateinit var map: MapView
+    private lateinit var lm : LocationManager
+    private lateinit var userNowLocation : Location
+    private var uLatitude by Delegates.notNull<Double>() // 위도
+    private var uLongitude by Delegates.notNull<Double>() // 경도
     private val marker = MapPOIItem()
+    private val db = Firebase.database
+    private val mAuth = Firebase.auth
+    val usersRef = db.getReference("user")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,35 +42,35 @@ class LocationFragment : Fragment(), MapView.MapViewEventListener {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLocationBinding.inflate(inflater, container, false)
+        lm = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
+        userNowLocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
+        uLatitude = userNowLocation?.latitude!!
+        uLongitude = userNowLocation?.longitude!!
+
+        val setBtn = binding!!.setBtn // 위치 설정 버튼
         val root : View = binding!!.root
         map = MapView(context)
-        //requestLocationUpdates()
+
         binding!!.mapView.addView(map)
-        startTracking()
-        map.setMapViewEventListener(this)
+        setBtn.setOnClickListener {
+            setLocation()
+        }
         // 줌인
         map.zoomIn(true)
         // 줌아웃
         map.zoomOut(true)
+        startTracking()
         return root
     }
 
     // 현재 사용자 위치 추적
     private fun startTracking() {
         map.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-        val lm = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        val userNowLocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-        // 위도, 경도
-        val uLatitude = userNowLocation?.latitude
-        val uLongitude = userNowLocation?.longitude
-        val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!)
-        Log.d("activity2", "latitude : $uLatitude, longitude : $uLongitude")
+        val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
         // 중심점 변경
-        map.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!), true)
-        //val uNowPosition = MapPoint.mapPointWithGeoCoord(37.5828, 127.0106)
-        //  map.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.5828, 127.0106), true)
-        Log.d("location", "위도: $uLatitude 경도: $uLongitude")
+        map.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude), true)
+        Log.d("myLocation", "위도: $uLatitude 경도: $uLongitude")
         // 현위치에 마커 찍기
         marker.itemName = "현 위치"
         marker.tag = 0
@@ -77,62 +84,27 @@ class LocationFragment : Fragment(), MapView.MapViewEventListener {
     private fun stopTasking() {
         map.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
     }
+
+    // 내 위치 설정
+    private fun setLocation() {
+        val geoCoder = Geocoder(context)
+
+        try {
+            val addresses = geoCoder.getFromLocation(uLatitude, uLongitude, 1)
+            if (addresses.isNotEmpty()){
+                val address = addresses[0]
+                val addressText = address.getAddressLine(0)
+                val addressString = addressText.drop(5)
+                // 내 위치 설정
+                usersRef.child(mAuth.currentUser?.uid!!).child("address").setValue(addressString)
+            }
+        }catch(e: IOException){
+            e.printStackTrace()
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         stopTasking()
-    }
-
-    private fun requestLocationUpdates() {
-        val locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
-
-        val locationListener = LocationListener { location ->
-            val latitude = location.latitude
-            val longitude = location.longitude
-            Log.d("activity", "latitude : $latitude, longitude : $longitude")
-        }
-
-        locationManager.requestLocationUpdates(
-            LocationManager.GPS_PROVIDER,
-            1000L,
-            10.0F,
-            locationListener
-        )
-    }
-
-    override fun onMapViewInitialized(p0: MapView?) {
-       Log.d("map", "onMapViewInitialized")
-    }
-
-    override fun onMapViewCenterPointMoved(p0: MapView?, p1: MapPoint?) {
-        Log.d("map", "onMapViewCenterPointMoved")
-    }
-
-    override fun onMapViewZoomLevelChanged(p0: MapView?, p1: Int) {
-        Log.d("map", "onMapViewZoomLevelChanged")
-    }
-
-    override fun onMapViewSingleTapped(p0: MapView?, p1: MapPoint?) {
-        Log.d("map", "onMapViewSingleTapped")
-        map.removePOIItem(marker)
-    }
-
-    override fun onMapViewDoubleTapped(p0: MapView?, p1: MapPoint?) {
-        Log.d("map", "onMapViewDoubleTapped")
-    }
-
-    override fun onMapViewLongPressed(p0: MapView?, p1: MapPoint?) {
-        Log.d("map", "onMapViewLongPressed")
-    }
-
-    override fun onMapViewDragStarted(p0: MapView?, p1: MapPoint?) {
-        Log.d("map", "onMapViewDragStarted")
-    }
-
-    override fun onMapViewDragEnded(p0: MapView?, p1: MapPoint?) {
-        Log.d("map", "onMapViewDragEnded")
-    }
-
-    override fun onMapViewMoveFinished(p0: MapView?, p1: MapPoint?) {
-        Log.d("map", "onMapViewMoveFinished")
     }
 }
