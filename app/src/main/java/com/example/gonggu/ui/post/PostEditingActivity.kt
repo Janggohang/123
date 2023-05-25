58f5900def33eaee4fb369e6bf1025b4e200e16d
hahahanDW <gksehddn8@naver.com>
2023-05-25 21:18:50 +0900

    게시글 수정 구현
    부족한 부분은 좀 더 알아보고 수정 예정

--- app/src/main/java/com/example/gonggu/ui/post/PostEditingActivity.kt
+++ app/src/main/java/com/example/gonggu/ui/post/PostEditingActivity.kt
@@ -12,19 +12,30 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.content.PermissionChecker
 import com.bumptech.glide.Glide
+import com.example.gonggu.MainActivity
 import com.example.gonggu.R
 import com.example.gonggu.databinding.ActivityPostEditingBinding
 import com.example.gonggu.databinding.ActivityPostViewer2Binding
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import java.text.SimpleDateFormat
+import java.util.Locale
 
+@Suppress("DEPRECATION")
 class PostEditingActivity : AppCompatActivity() {
 
     private lateinit var mDbRef: DatabaseReference
     private var selectedUri: Uri? = null
-    val binding by lazy { ActivityPostEditingBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityPostEditingBinding.inflate(layoutInflater) }
+    private val storage: FirebaseStorage by lazy { Firebase.storage }
     private val DEFAULT_GALLERY_CODE = 2020
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -37,6 +48,48 @@ class PostEditingActivity : AppCompatActivity() {
 
         // 게시물의 기존 정보 불러오기
         loadPostData()
+
+        binding.photoButton.setOnClickListener {
+            when {
+                PermissionChecker.checkSelfPermission(
+                    this@PostEditingActivity,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) == PermissionChecker.PERMISSION_GRANTED
+                -> {
+                    startContentProvider()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_MEDIA_IMAGES) -> {
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+                        1000)
+                }
+            }
+        }
+
+        // 게시물 수정 완료
+        binding.submitBtn.setOnClickListener{
+            // 선택된 사진이 있는 경우
+            if (selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+                uploadPhoto(photoUri,
+                    successHandler = { uri ->
+                        editingPost(uri)
+                    },
+                    errorHandler = {
+                        Toast.makeText(this@PostEditingActivity,
+                            "사진 업로드에 실패했습니다!!!",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                )
+            } else {
+                editingPost("")
+            }
+            finish()
+            PostViewerActivity.currentPost = currentPost
+        }
     }
 
     private fun loadPostData() {
@@ -58,6 +111,61 @@ class PostEditingActivity : AppCompatActivity() {
         binding.contentEdit.setText(currentPost.content)
     }
 
+    private fun editingPost(imageUri: String) {
+        val title = binding.titleEdit.text.toString() // 제목
+        val price = binding.priceEdit.text.toString().toIntOrNull() // 가격
+        val numOfPeople = binding.countEdit.text.toString().toIntOrNull() // 인원 수
+        val content = binding.contentEdit.text.toString() // 내용
+        val location = binding.myLocation.text.toString() // 위치
+        val pricePerPerson = binding.pricePerText.text.toString().toIntOrNull() // 인당 가격
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()) // 포맷 지정
+        val currentTime = System.currentTimeMillis() // 현재 시간
+        val time = dateFormat.format(currentTime) // 현재 시간을 포맷에 맞게 변환
+
+        if (title.isEmpty() || price == null ||
+            numOfPeople == null || content.isEmpty() || location == null) {
+            // 필수 입력값이 빠졌을 때
+            Toast.makeText(this@PostEditingActivity,
+                "모든 항목을 입력해 주세요.",
+                Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        mDbRef = Firebase.database.reference
+        val postRef = mDbRef.child("post").child(PostViewerActivity.currentPost.postId)
+        // 게시글 수정
+        postRef.get().addOnSuccessListener{ dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    val postData = dataSnapshot.value as? HashMap<String, Any>
+                    postData?.let { data ->
+                        data["title"] = title
+                        data["price"] = price
+                        data["imageUri"] = imageUri
+                        data["numOfPeople"] = numOfPeople
+                        data["content"] = content
+                        data["pricePerPerson"] = pricePerPerson!!
+                        data["time"] = time
+
+                        postRef.updateChildren(data)
+                            .addOnSuccessListener {
+                                Toast.makeText(
+                                    this@PostEditingActivity,
+                                    "게시글 수정이 완료됐습니다.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            .addOnFailureListener { e ->
+                                Toast.makeText(
+                                    this@PostEditingActivity,
+                                    "게시글 수정 실패: $e",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                    }
+                }
+            }
+    }
+
     private inner class MyTextWatcher: TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             // 텍스트 변경 전에 호출
@@ -77,6 +185,26 @@ class PostEditingActivity : AppCompatActivity() {
             // 텍스트 변경 후에 호출
         }
 
+    }
+
+    //storage에 사진 업로드 함수
+    private fun uploadPhoto(uri: Uri, successHandler: (String) -> Unit, errorHandler: () -> Unit) {
+        val fileName = "${System.currentTimeMillis()}.png"
+        storage.reference.child("gonggu/photo").child(fileName)
+            .putFile(uri)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    storage.reference.child("gonggu/photo").child(fileName)
+                        .downloadUrl
+                        .addOnSuccessListener { uri ->
+                            successHandler(uri.toString())
+                        }.addOnFailureListener {
+                            errorHandler()
+                        }
+                } else {
+                    errorHandler()
+                }
+            }
     }
 
     private fun startContentProvider() {
