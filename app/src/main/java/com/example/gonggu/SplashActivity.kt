87dadbd9b67ecb982ca25c9a31360a9514a667f9
최민영 <choimy1996@gmail.com>
2023-05-31 21:59:31 +0900

    ddd

--- app/src/main/java/com/example/gonggu/SplashActivity.kt
+++ app/src/main/java/com/example/gonggu/SplashActivity.kt
@@ -19,22 +19,9 @@ import android.view.View
 import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
-import android.widget.Toast
-import androidx.annotation.RequiresApi
-import java.security.MessageDigest
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.gonggu.databinding.ActivityChatBinding
-import com.example.gonggu.databinding.ActivityMainBinding
+
 import com.example.gonggu.databinding.ActivitySplashBinding
-import com.example.gonggu.ui.chat.ChatFragment
-import com.example.gonggu.ui.heart.HeartFragment
-import com.example.gonggu.ui.home.HomeFragment
-import com.example.gonggu.ui.post.DeliveryPostFragment
-import com.example.gonggu.ui.post.PostFragment
-import com.example.gonggu.ui.profile.ProfileFragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.gonggu.ui.login.LoginActivity
 
 
 class SplashActivity : AppCompatActivity() {
@@ -47,18 +34,6 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
         splashAnimation()
-
-
-
-//        Handler().postDelayed({
-//            // Splash 화면이 표시된 후 수행할 작업을 여기에 작성합니다.
-//            // 예: 다음 화면으로 이동하는 Intent 등
-//            startActivity(Intent(this, MainActivity::class.java))
-//            finish()
-//        }, 3000) // 3초(3000 밀리초) 딜레이 설정
-//
-
-
     }
     private fun splashAnimation() {
         val imageAnim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_imageview)
@@ -66,7 +41,7 @@ class SplashActivity : AppCompatActivity() {
 
         imageAnim.setAnimationListener(object : AnimationListener {
             override fun onAnimationEnd(animation: Animation?) {
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                val intent = Intent(this@SplashActivity, LoginActivity::class.java)
                 startActivity(intent)
                 overridePendingTransition(R.anim.anim_splash_out_top, R.anim.anim_splash_in_down)
                 finish()
@@ -80,7 +55,6 @@ class SplashActivity : AppCompatActivity() {
             override fun onAnimationRepeat(animation: Animation?) {
                 TODO("Not yet implemented")
             }
-
 
         })
     }
