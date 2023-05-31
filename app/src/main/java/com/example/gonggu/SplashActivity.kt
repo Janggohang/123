92f7975cd2420f50ce0d8a0d2ba466291a81e49f
최민영 <choimy1996@gmail.com>
2023-05-31 21:49:19 +0900

    splash 이미지

--- app/src/main/java/com/example/gonggu/SplashActivity.kt
+++ app/src/main/java/com/example/gonggu/SplashActivity.kt
@@ -17,6 +17,7 @@ import android.os.PersistableBundle
 import android.util.Base64
 import android.view.View
 import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -24,7 +25,9 @@ import java.security.MessageDigest
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.gonggu.databinding.ActivityChatBinding
 import com.example.gonggu.databinding.ActivityMainBinding
+import com.example.gonggu.databinding.ActivitySplashBinding
 import com.example.gonggu.ui.chat.ChatFragment
 import com.example.gonggu.ui.heart.HeartFragment
 import com.example.gonggu.ui.home.HomeFragment
@@ -35,16 +38,51 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.splash_base)
+        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        splashAnimation()
 
-        Handler().postDelayed({
-            // Splash 화면이 표시된 후 수행할 작업을 여기에 작성합니다.
-            // 예: 다음 화면으로 이동하는 Intent 등
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000) // 3초(3000 밀리초) 딜레이 설정
+
+
+//        Handler().postDelayed({
+//            // Splash 화면이 표시된 후 수행할 작업을 여기에 작성합니다.
+//            // 예: 다음 화면으로 이동하는 Intent 등
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }, 3000) // 3초(3000 밀리초) 딜레이 설정
+//
+
+
+    }
+    private fun splashAnimation() {
+        val imageAnim = AnimationUtils.loadAnimation(this,R.anim.anim_splash_imageview)
+        binding.splash.startAnimation(imageAnim)
+
+        imageAnim.setAnimationListener(object : AnimationListener {
+            override fun onAnimationEnd(animation: Animation?) {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.anim_splash_out_top,R.anim.anim_splash_in_down)
+                finish()
+            }
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
+
     }
 }
