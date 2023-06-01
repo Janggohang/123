3f0acd98e61fe814665e433e1d194cb58789ad65
최민영 <choimy1996@gmail.com>
2023-06-01 21:18:56 +0900

    배달 게시글 ui 수정

--- app/src/main/java/com/example/gonggu/ui/post/DeliveryViewerActivity.kt
+++ app/src/main/java/com/example/gonggu/ui/post/DeliveryViewerActivity.kt
@@ -66,9 +66,9 @@ class DeliveryViewerActivity : AppCompatActivity() {
 
         binding.dateText.text = currentDelivery.time // 시간
         binding.postTitle.text = currentDelivery.title // 제목
-        binding.postLocation.text = currentDelivery.location // 위치
+        binding.postLocation.text = "위치 : ${ currentDelivery.location }"
         binding.priceText.text = "${currentDelivery.price}￦" // 가격
-        binding.numOfPeopleText.text = "${currentDelivery.numOfPeople}명" // 인원 수
+        binding.numOfPeopleText.text = "${currentDelivery.joiner.size}/${currentDelivery.numOfPeople}명" // 인원 수
         binding.pricePerPersonText.text = "${currentDelivery.pricePerPerson}￦" // 인당 가격
         binding.likeCount.text = currentDelivery.like.size.toString() // 좋아요
         binding.category.text = currentDelivery.category // 카테고리
